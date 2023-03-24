a1636755d0484080896b0e5f5016f74026467650
cozy <wonje1108@gmail.com>
2023-03-24 17:58:34 +0900

    v1

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.r6mbvxn.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/guestbook", methods=["POST"])
+def guestbook_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive
+    }
+    db.fans.insert_one(doc)
+    return jsonify({'msg': '등록 완료.'})
+
+@app.route("/guestbook", methods=["GET"])
+def guestbook_get():
+    all_comments = list(db.fans.find({},{'_id':False}))
+    return jsonify({'result': all_comments})
+
+@app.route("/guestbook/delete", methods=["DELETE"])
+def guestbook_delete():
+    name_receive = request.form['name_give']
+    db.fans.delete_one({'name':name_receive})
+    return jsonify({'msg': '잉.. 지우지 말지..'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
